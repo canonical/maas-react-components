f761cd618d20eda1126db935082f8f70ba1b2915
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-23 12:35:51 +0100

    fix: adjust navigation banner

--- src/lib/sections/Navigation/Navigation.test.tsx
+++ src/lib/sections/Navigation/Navigation.test.tsx
@@ -3,6 +3,6 @@ import { render, screen } from "@testing-library/react";
 import { Navigation } from "./Navigation";
 
 it("renders without crashing", () => {
-  render(<Navigation />);
+  render(<Navigation isCollapsed={false}>Navigation component</Navigation>);
   expect(screen.getByText("Navigation component")).toBeInTheDocument();
 });
