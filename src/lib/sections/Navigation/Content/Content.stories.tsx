c73730de75295174dcc0a3e6bb09cff3b9901b70
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-24 11:53:34 +0100

    docs: add more items to content story

--- src/lib/sections/Navigation/Content/Content.stories.tsx
+++ src/lib/sections/Navigation/Content/Content.stories.tsx
@@ -24,18 +24,34 @@ const meta: Meta<typeof Navigation.Content> = {
 
 export default meta;
 export const Example = { args: { children: (
-  <Navigation.List>
-    <Navigation.Item>
-      <Navigation.Link to="#" current="page">
-        <Navigation.Icon name="code" />
-        <Navigation.Label>Settings</Navigation.Label>
-      </Navigation.Link>
-    </Navigation.Item>
-    <Navigation.Item>
-      <Navigation.Link to="#">
-        <Navigation.Icon name="user" />
-        <Navigation.Label>Account</Navigation.Label>
-      </Navigation.Link>
-    </Navigation.Item>
-  </Navigation.List>
+  <>
+    <Navigation.List>
+      <Navigation.Item>
+        <Navigation.Link to="#" current="page">
+          <Navigation.Icon name="code" />
+          <Navigation.Label>Settings</Navigation.Label>
+        </Navigation.Link>
+      </Navigation.Item>
+      <Navigation.Item>
+        <Navigation.Link to="#">
+          <Navigation.Icon name="user" />
+          <Navigation.Label>Account</Navigation.Label>
+        </Navigation.Link>
+      </Navigation.Item>
+    </Navigation.List>
+    <Navigation.List>
+      <Navigation.Item>
+        <Navigation.Link to="#">
+          <Navigation.Icon name="information" />
+          <Navigation.Label>About</Navigation.Label>
+        </Navigation.Link>
+      </Navigation.Item>
+      <Navigation.Item>
+        <Navigation.Link to="#">
+          <Navigation.Icon name="help" />
+          <Navigation.Label>Get support</Navigation.Label>
+        </Navigation.Link>
+      </Navigation.Item>
+    </Navigation.List>
+  </>
 )}};
