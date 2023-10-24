25c2dd5bdbe94e26409aea7ebd1a69337f78c7ec
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-24 13:16:10 +0100

    docs: add story for label

---
+++ src/lib/sections/Navigation/Label/Label.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta } from "@storybook/react";
+
+import { Navigation } from "@/lib/sections/Navigation/Navigation";
+
+const meta: Meta<typeof Navigation.Label> = {
+  title: "Sections/Navigation/Label",
+  component: Navigation.Label,
+  render: (args) => (
+    <Navigation isCollapsed={false}>
+      <Navigation.Drawer>
+        <Navigation.Content>
+          <Navigation.List>
+            <Navigation.Item>
+              <Navigation.Link to="#">
+                <Navigation.Label>
+                  {args["children"]}
+                </Navigation.Label>
+              </Navigation.Link>
+            </Navigation.Item>
+          </Navigation.List>
+        </Navigation.Content>
+      </Navigation.Drawer>
+    </Navigation>
+  ),
+  tags: ["autodocs"],
+  parameters: {
+    status: {
+      type: "candidate",
+    },
+  },
+};
+
+export default meta;
+export const Example = { args: { children: "Settings"}};
