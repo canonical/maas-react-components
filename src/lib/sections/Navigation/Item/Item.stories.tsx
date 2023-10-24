c96aadcd9aa39af1872697f4175e58254327f145
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-24 13:12:45 +0100

    docs: add story for item

---
+++ src/lib/sections/Navigation/Item/Item.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta } from "@storybook/react";
+
+import { Navigation } from "@/lib/sections/Navigation/Navigation";
+
+const meta: Meta<typeof Navigation.Item> = {
+  title: "Sections/Navigation/Item",
+  component: Navigation.Item,
+  render: (args) => (
+    <Navigation isCollapsed={false}>
+      <Navigation.Drawer>
+        <Navigation.Content>
+          <Navigation.List>
+            <Navigation.Item>
+              {args["children"]}
+            </Navigation.Item>
+          </Navigation.List>
+        </Navigation.Content>
+      </Navigation.Drawer>
+    </Navigation>
+  ),
+  tags: ["autodocs"],
+  parameters: {
+    status: {
+      type: "candidate",
+    },
+  },
+};
+
+export default meta;
+export const Example = { args: { children: (
+  <Navigation.Link to="#">
+    <Navigation.Icon name="code" />
+    <Navigation.Label>Settings</Navigation.Label>
+  </Navigation.Link>
+)}};
