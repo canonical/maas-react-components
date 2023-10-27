8ffb8126f93e2e2f4546414b59918f83dbeedde4
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-27 14:39:13 +0100

    feat: add classname prop to menu button

--- src/lib/sections/Navigation/MenuButton/MenuButton.tsx
+++ src/lib/sections/Navigation/MenuButton/MenuButton.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from "react";
 
 import { Button, ButtonProps } from "@canonical/react-components";
+import classNames from "classnames";
 
 export interface NavigationMenuButtonProps {
   children: ReactNode;
+  className?: string;
   onClick: ButtonProps["onClick"];
 }
 
-export const MenuButton = ({ children, onClick }: NavigationMenuButtonProps) => {
+export const MenuButton = ({ children, className, onClick }: NavigationMenuButtonProps) => {
 
   return (
-    <Button appearance="base" className="has-icon is-dark" onClick={onClick}>
+    <Button appearance="base" className={classNames("has-icon is-dark", className)} onClick={onClick}>
       {children}
     </Button>
   )
