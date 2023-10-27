fd7d5634b869161594c5d6dd1384aa78a7db6bf1
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-27 14:34:43 +0100

    feat: add classname prop to item

--- src/lib/sections/Navigation/Item/Item.tsx
+++ src/lib/sections/Navigation/Item/Item.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from "react";
 
+import classNames from "classnames";
+
 export interface NavigationItemProps {
   children: ReactNode;
+  className?: string;
 }
 
-export const Item = ({ children }: NavigationItemProps) => {
+export const Item = ({ children, className }: NavigationItemProps) => {
   return (
-    <li className="p-side-navigation__item">
+    <li className={classNames("p-side-navigation__item", className)}>
       {children}
     </li>
   )
