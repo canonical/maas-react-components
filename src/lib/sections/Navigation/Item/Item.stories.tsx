5e062da92682351e940358e73d0ad6059c4d3ca6
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-27 15:12:36 +0100

    docs: add group example to item story

--- src/lib/sections/Navigation/Item/Item.stories.tsx
+++ src/lib/sections/Navigation/Item/Item.stories.tsx
@@ -10,9 +10,7 @@ const meta: Meta<typeof Navigation.Item> = {
       <Navigation.Drawer>
         <Navigation.Content>
           <Navigation.List>
-            <Navigation.Item>
-              {args["children"]}
-            </Navigation.Item>
+            <Navigation.Item {...args}/>
           </Navigation.List>
         </Navigation.Content>
       </Navigation.Drawer>
@@ -32,4 +30,26 @@ export const Example = { args: { children: (
     <Navigation.Icon name="code" />
     <Navigation.Label>Settings</Navigation.Label>
   </Navigation.Link>
-)}};
+), hasActiveChild: false }};
+
+export const GroupExample = { args: { children: (
+  <>
+    <Navigation.Text>
+      <Navigation.Icon name="machines"/>
+      <Navigation.Label variant="group">Hardware</Navigation.Label>
+    </Navigation.Text>
+    <Navigation.List>
+      <Navigation.Item>
+        <Navigation.Link href="#" aria-current="page">
+          <Navigation.Label>Machines</Navigation.Label>
+        </Navigation.Link>
+        <Navigation.Link href="#">
+          <Navigation.Label>Controllers</Navigation.Label>
+        </Navigation.Link>
+        <Navigation.Link href="#">
+          <Navigation.Label>Devices</Navigation.Label>
+        </Navigation.Link>
+      </Navigation.Item>
+    </Navigation.List>
+  </>
+), hasActiveChild: true }}
