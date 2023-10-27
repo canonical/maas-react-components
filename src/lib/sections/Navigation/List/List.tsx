5bdcbd58987c63b6eed73af9141fed636dbafa61
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-27 14:35:21 +0100

    feat: add classname prop to list

--- src/lib/sections/Navigation/List/List.tsx
+++ src/lib/sections/Navigation/List/List.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from "react";
 
+import classNames from "classnames";
+
 export interface NavigationListProps {
   children: ReactNode;
+  className?: string;
 }
 
-export const List = ({ children }: NavigationListProps) => {
+export const List = ({ children, className }: NavigationListProps) => {
   return (
-    <ul className="p-side-navigation__list">
+    <ul className={classNames("p-side-navigation__list", className)}>
       {children}
     </ul>
   )
