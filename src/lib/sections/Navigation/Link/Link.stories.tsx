230e2e902f619f18a5678a779bbda1503b608c1a
Jones Ogolo <jay.topher11@gmail.com>
2023-10-27 12:46:03 +0100

    feat: add button link example

--- src/lib/sections/Navigation/Link/Link.stories.tsx
+++ src/lib/sections/Navigation/Link/Link.stories.tsx
@@ -40,3 +40,14 @@ export const Example = {
     ),
   },
 };
+
+export const ButtonExample = {
+  args: {
+    as: "button",
+    children: (
+      <>
+        <Navigation.Label>Log out</Navigation.Label>
+      </>
+    ),
+  },
+};
