c18c9748503db7aae7985392ac54f9a8af699f95
Nick De Villiers <nick.devilliers@canonical.com>
2023-11-03 11:41:07 +0000

    feat: add id prop to label

--- src/lib/sections/Navigation/Label/Label.tsx
+++ src/lib/sections/Navigation/Label/Label.tsx
@@ -4,13 +4,14 @@ import classNames from "classnames";
 
 export interface NavigationLabelProps {
   children: ReactNode;
+  id?: string;
   variant?: "base" | "group";
 }
 
-export const Label = ({ children, variant="base" }: NavigationLabelProps) => {
+export const Label = ({ children, id, variant="base" }: NavigationLabelProps) => {
 
   return (
-    <span className={classNames("p-side-navigation__label", { "p-side-navigation__label--group": variant === "group" })}>
+    <span className={classNames("p-side-navigation__label", { "p-side-navigation__label--group": variant === "group" })} id={id}>
       {children}
     </span>
   )
