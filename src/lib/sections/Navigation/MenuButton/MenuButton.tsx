71b9ff5abec886f1336974ee7d994dccec6f1d9c
Nick De Villiers <nick.devilliers@canonical.com>
2023-11-08 13:19:49 +0200

    fix: remove focus from menu button after click (#34)

--- src/lib/sections/Navigation/MenuButton/MenuButton.tsx
+++ src/lib/sections/Navigation/MenuButton/MenuButton.tsx
@@ -9,11 +9,29 @@ export interface NavigationMenuButtonProps {
   onClick: ButtonProps["onClick"];
 }
 
-export const MenuButton = ({ children, className, onClick }: NavigationMenuButtonProps) => {
-
+export const MenuButton = ({
+  children,
+  className,
+  onClick,
+}: NavigationMenuButtonProps) => {
   return (
-    <Button appearance="base" className={classNames("p-side-navigation__button--menu has-icon is-dark", className)} onClick={onClick}>
+    <Button
+      appearance="base"
+      className={classNames(
+        "p-side-navigation__button--menu has-icon is-dark",
+        className,
+      )}
+      onClick={(e) => {
+        if (onClick) {
+          onClick(e);
+        }
+        // Make sure the button does not have focus
+        // .l-navigation remains open with :focus-within
+        e.stopPropagation();
+        e.currentTarget.blur();
+      }}
+    >
       {children}
     </Button>
-  )
-}
+  );
+};
