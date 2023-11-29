20923e0fe64e9f24da2f4069dc3885e1cc8d5acf
Nick De Villiers <nick.devilliers@canonical.com>
2023-11-29 11:00:36 +0000

    docs: create controlled example

--- src/lib/components/Pagination/Pagination.stories.tsx
+++ src/lib/components/Pagination/Pagination.stories.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 import { Meta } from "@storybook/react";
 
-import { Pagination } from "./Pagination";
+import { Pagination, PaginationProps } from "./Pagination";
 
 const meta: Meta<typeof Pagination> = {
   title: "Components/Pagination",
@@ -16,6 +18,55 @@ const meta: Meta<typeof Pagination> = {
   },
 };
 
+const ControlledTemplate = () => {
+  const totalPages = 10;
+  const disabled = false;
+
+  const [pageNumber, setPageNumber] = useState(1);
+  const [error, setError] = useState("");
+
+  const onInputBlur = () => {
+    setPageNumber(pageNumber);
+    setError("");
+  };
+
+  const onInputChange: PaginationProps["onInputChange"] = (e) => {
+    const { value, valueAsNumber } = e.target;
+    if (value) {
+      setPageNumber(valueAsNumber);
+      if (valueAsNumber > totalPages || valueAsNumber < 1) {
+        setError(`${valueAsNumber} is not a valid page number.`);
+      } else {
+        setError("");
+      }
+    } else {
+      setError("Enter a page number.");
+    }
+  };
+
+  const onPreviousClick = () => {
+    setPageNumber((page) => Number(page) - 1);
+  };
+
+  const onNextClick = () => {
+    setPageNumber((page) => Number(page) + 1);
+  };
+
+  return (
+    <Pagination 
+      error={error}
+      disabled={disabled}
+      onInputBlur={onInputBlur}
+      onInputChange={onInputChange}
+      onNextClick={onNextClick}
+      onPreviousClick={onPreviousClick}
+      pageNumber={pageNumber}
+      totalPages={totalPages}
+    />
+  )
+
+}
+
 export default meta;
 export const Example = { 
   args: {
@@ -28,4 +79,15 @@ export const Example = {
     pageNumber: 1,
     totalPages: 10,
   }, 
+};
+
+export const ControlledExample = {
+  render: ControlledTemplate,
+  parameters: {
+    docs: {
+      source: {
+        code: null,
+      },
+    },
+  },
 };
