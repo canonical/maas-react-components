3d75e9501ad488c168d4dc7d23985dfc4d2fe254
Peter Makowski <me+git@petermakowski.io>
2024-01-03 13:19:28 +0100

    feat: add stepper MAASENG-2535 - disable scss/at-extend-no-missing-placeholder

---
+++ src/lib/components/Stepper/Stepper.tsx
@@ -0,0 +1,39 @@
+import classNames from "classnames";
+import "./Stepper.scss";
+
+type Props = {
+  /**
+   * A zero-based index of the currently active step
+   */
+  activeStep: number;
+  /**
+   * An array of strings representing individual steps
+   */
+  items: string[];
+};
+
+export const Stepper = ({ activeStep, items }: Props): JSX.Element => {
+  return (
+    <ol className="stepper">
+      {items.map((step, i) => {
+        const isActive = i === activeStep;
+        const isComplete = i < activeStep;
+        return (
+          <li aria-label={step} className="stepper__item" key={i}>
+            <p
+              aria-label={isComplete ? `${step} (completed)` : step}
+              className={classNames("stepper__title", {
+                "stepper__title--is-active": isActive,
+                "stepper__title--is-complete": isComplete,
+              })}
+            >
+              {step}
+            </p>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
+
+export default Stepper;
