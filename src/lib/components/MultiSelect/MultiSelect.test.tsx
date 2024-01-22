5fe040122346211fcbbcb07058f039f440e5aac8
Nick De Villiers <nick.devilliers@canonical.com>
2024-01-22 14:09:31 +0000

    feat(MultiSelect): update MultiSelectItem type to be label+value object MAASENG-2613 (#63)
    Updated `MultiSelectItem` to be `{ label: string; value: string | number }`

--- src/lib/components/MultiSelect/MultiSelect.test.tsx
+++ src/lib/components/MultiSelect/MultiSelect.test.tsx
@@ -4,21 +4,21 @@ import { vi } from "vitest";
 
 import { MultiSelect } from "./MultiSelect";
 
-const items = ["item one", "item two"];
+const items = [{ label: "item one", value: 1 }, { label: "item two", value: 2 }];
 
 it("shows options when opened", async () => {
   render(<MultiSelect items={items} />);
 
   items.forEach((item) => {
     expect(
-      screen.queryByRole("checkbox", { name: item }),
+      screen.queryByRole("checkbox", { name: item.label }),
     ).not.toBeInTheDocument();
   });
 
   await userEvent.click(screen.getByRole("combobox"));
 
   items.forEach((item) => {
-    expect(screen.queryByRole("checkbox", { name: item })).toBeInTheDocument();
+    expect(screen.queryByRole("checkbox", { name: item.label })).toBeInTheDocument();
   });
 });
 
@@ -31,26 +31,26 @@ it("opens the dropdown when the combobox is clicked", async () => {
 
 it("can have some options preselected", async () => {
   render(<MultiSelect items={items} selectedItems={[items[0]]} />);
-  expect(screen.getByRole("listitem", { name: items[0] })).toBeInTheDocument();
+  expect(screen.getByRole("listitem", { name: items[0].label })).toBeInTheDocument();
   expect(
-    screen.queryByRole("checkbox", { name: items[0] }),
+    screen.queryByRole("checkbox", { name: items[0].label }),
   ).not.toBeInTheDocument();
   await userEvent.click(screen.getByRole("combobox"));
-  expect(screen.getByRole("checkbox", { name: items[0] })).toBeInTheDocument();
+  expect(screen.getByRole("checkbox", { name: items[0].label })).toBeInTheDocument();
 });
 
 it("can select options from the dropdown", async () => {
   const onItemsUpdate = vi.fn();
   render(<MultiSelect items={items} onItemsUpdate={onItemsUpdate} />);
   await userEvent.click(screen.getByRole("combobox"));
-  await userEvent.click(screen.getByLabelText(items[0]));
+  await userEvent.click(screen.getByLabelText(items[0].label));
   await waitFor(() => expect(onItemsUpdate).toHaveBeenCalledWith([items[0]]));
 });
 
 it("can hide the options that have been selected", async () => {
   render(<MultiSelect items={items} />);
   await userEvent.click(screen.getByRole("combobox"));
-  await userEvent.click(screen.getByLabelText(items[0]));
+  await userEvent.click(screen.getByLabelText(items[0].label));
   expect(screen.queryByTestId("selected-option")).not.toBeInTheDocument();
 });
 
@@ -66,13 +66,13 @@ it("can remove options that have been selected", async () => {
   expect(screen.getAllByRole("listitem", { name: /item/ })).toHaveLength(2);
   await userEvent.click(screen.getByRole("combobox"));
   await userEvent.click(
-    within(screen.getByRole("listbox")).getByLabelText(items[0]),
+    within(screen.getByRole("listbox")).getByLabelText(items[0].label),
   );
   expect(onItemsUpdate).toHaveBeenCalledWith([items[1]]);
 });
 
 it("can filter option list", async () => {
-  render(<MultiSelect items={[...items, "other"]} />);
+  render(<MultiSelect items={[...items, { label: "other", value: 3 }]} />);
   await userEvent.click(screen.getByRole("combobox"));
   expect(screen.getAllByRole("listitem")).toHaveLength(3);
   await userEvent.type(screen.getByRole("combobox"), "item");
