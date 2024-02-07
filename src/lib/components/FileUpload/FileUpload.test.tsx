a2f415d78dfefbce37f2bfb8da696e7112dae4c6
Nick De Villiers <nick.devilliers@canonical.com>
2024-02-07 15:20:23 +0000

    feat(FileUpload): verify file upload component MAASENG-2588 (#100)
    Co-authored-by: Peter Makowski <peter.makowski@canonical.com>

--- src/lib/components/FileUpload/FileUpload.test.tsx
+++ src/lib/components/FileUpload/FileUpload.test.tsx
@@ -1,25 +1,38 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { vi } from "vitest";
 
-import { FileUpload } from "./FileUpload";
+import { FileUpload, FileUploadProps } from "./FileUpload";
 beforeEach(() => {});
 
-function createDataTransfer(file: File) {
+function createDataTransfer(files: File[]) {
   return {
-    files: [file],
-    items: [
-      {
-        kind: "file",
-        type: file.type,
-        getAsFile: () => file,
-      },
-    ],
+    files: files,
+    items: files.map((file) => ({
+      kind: "file",
+      type: file.type,
+      getAsFile: () => file,
+    })),
     types: ["Files"],
   };
 }
 
+const renderFileUpload = (options?: Partial<FileUploadProps>) => {
+  return render(
+    <FileUpload
+      files={[]}
+      onFileUpload={vi.fn()}
+      rejectedFiles={[]}
+      removeFile={vi.fn()}
+      removeRejectedFile={vi.fn()}
+      {...options}
+    />,
+  );
+};
+
 it("renders without crashing", () => {
-  render(<FileUpload onFileUpload={vi.fn()} />);
+  renderFileUpload();
+
   expect(
     screen.getByText("Drag and drop files here or click to upload"),
   ).toBeInTheDocument();
@@ -27,12 +40,12 @@ it("renders without crashing", () => {
 
 it("calls onFileUpload when files are dropped", async () => {
   const mockOnFileUpload = vi.fn();
-  render(<FileUpload onFileUpload={mockOnFileUpload} />);
+  renderFileUpload({ onFileUpload: mockOnFileUpload });
   const file = new File(["hello"], "hello.png", { type: "image/png" });
   const dropEvent = await fireEvent.drop(
     screen.getByText("Drag and drop files here or click to upload"),
     {
-      dataTransfer: createDataTransfer(file),
+      dataTransfer: createDataTransfer([file]),
     },
   );
 
@@ -44,4 +57,82 @@ it("calls onFileUpload when files are dropped", async () => {
       expect.anything(),
     ),
   );
+});
+
+it("displays accepted files", () => {
+  const file = new File(["hello"], "hello.png", { type: "image/png" });
+
+  renderFileUpload({ files: [file] });
+
+  expect(screen.getByText(file.name)).toBeInTheDocument();
+});
+
+it("calls removeFile when the 'remove' button is clicked", async () => {
+  const file = new File(["hello"], "hello.png", { type: "image/png" });
+  const mockRemoveFile = vi.fn();
+
+  renderFileUpload({ files: [file], removeFile: mockRemoveFile });
+
+  await userEvent.click(screen.getByRole("button", { name: /Remove/i }));
+
+  expect(mockRemoveFile).toHaveBeenCalledWith(file);
+});
+
+it("displays rejected files and reasons", () => {
+  const file = new File(["hello"], "hello.png", { type: "image/png" });
+  const rejection = {
+    file,
+    errors: [{ code: "an-error-code", message: "This is an error" }],
+  };
+
+  renderFileUpload({ rejectedFiles: [rejection] });
+
+  expect(screen.getByText(file.name)).toBeInTheDocument();
+  expect(screen.getByText("This is an error")).toBeInTheDocument();
+});
+
+it("calls removeRejectedFile when the 'remove' button is clicked", async () => {
+  const file = new File(["hello"], "hello.png", { type: "image/png" });
+  const rejection = {
+    file,
+    errors: [{ code: "an-error-code", message: "This is an error" }],
+  };
+  const mockRemoveRejectedFile = vi.fn();
+
+  renderFileUpload({
+    rejectedFiles: [rejection],
+    removeRejectedFile: mockRemoveRejectedFile,
+  });
+
+  await userEvent.click(screen.getByRole("button", { name: /Remove/i }));
+
+  expect(mockRemoveRejectedFile).toHaveBeenCalledWith(rejection);
+});
+
+it("hides the drop zone when the maximum number of files is met", () => {
+  const file = new File(["hello"], "hello.png", { type: "image/png" });
+
+  renderFileUpload({ files: [file], maxFiles: 1 });
+
+  expect(
+    screen.queryByText("Drag and drop files here or click to upload"),
+  ).not.toBeInTheDocument();
+});
+
+it("can display errors on the dropzone", () => {
+  renderFileUpload({ error: "This is an error" });
+
+  expect(screen.getByText("This is an error")).toBeInTheDocument();
+});
+
+it("can display a label", () => {
+  renderFileUpload({ label: "Upload files" });
+
+  expect(screen.getByLabelText("Upload files")).toBeInTheDocument();
+});
+
+it("can display help text", () => {
+  renderFileUpload({ help: "Some helpful text" });
+
+  expect(screen.getByText("Some helpful text")).toBeInTheDocument();
 });
--- src/lib/components/FileUpload/FileUpload.tsx
+++ src/lib/components/FileUpload/FileUpload.tsx
@@ -1,23 +1,167 @@
-import { useDropzone, DropzoneOptions } from "react-dropzone";
+import { ReactNode, useCallback, useId, useState } from "react";
 
-export interface FileUploadProps {
-  onFileUpload: NonNullable<DropzoneOptions["onDrop"]>;
-}
-
+import { Button, Icon, Label } from "@canonical/react-components";
+import classNames from "classnames";
+import { useDropzone, DropzoneOptions, FileRejection } from "react-dropzone";
 import "./FileUpload.scss";
 
+export interface FileUploadProps {
+  accept?: DropzoneOptions["accept"];
+  error?: ReactNode;
+  files: File[];
+  help?: string;
+  label?: string;
+  maxFiles?: number;
+  maxSize?: number;
+  onFileUpload: NonNullable<DropzoneOptions["onDrop"]>;
+  rejectedFiles: FileRejection[];
+  removeFile: (file: File) => void;
+  removeRejectedFile: (fileRejection: FileRejection) => void;
+}
+
 export const FileUpload: React.FC<FileUploadProps> = ({
+  accept,
+  error,
+  files,
+  help,
+  label,
+  maxFiles,
+  maxSize,
   onFileUpload,
+  rejectedFiles,
+  removeFile,
+  removeRejectedFile,
 }: FileUploadProps) => {
   const { getRootProps } = useDropzone({
+    accept,
+    maxFiles,
+    maxSize,
     onDrop: onFileUpload,
   });
 
+  const labelId = useId();
+
   return (
-    <div {...getRootProps()} className="file-upload" data-testid="file-upload">
-      <button className="file-upload__button" type="button">
-        Drag and drop files here or click to upload
-      </button>
+    <div
+      className={classNames("p-form__group p-form-validation", {
+        "is-error": !!error,
+      })}
+    >
+      {label && <Label id={labelId}>{label}</Label>}
+      {help && <p className="p-form-help-text">{help}</p>}
+      <div className="p-form__control">
+        {!maxFiles || files.length < maxFiles ? (
+          <div className="file-upload__wrapper">
+            <div
+              {...getRootProps()}
+              aria-labelledby={label ? labelId : undefined}
+              className="file-upload"
+              data-testid="file-upload"
+            >
+              <button className="file-upload__button" type="button">
+                Drag and drop files here or click to upload
+              </button>
+            </div>
+          </div>
+        ) : null}
+        {error ? (
+          <p className="p-form-validation__message">
+            <strong>Error: </strong>
+            {error}
+          </p>
+        ) : null}
+        <div className="file-upload__files-list">
+          {rejectedFiles &&
+            rejectedFiles.map((rejection) => (
+              <span className="is-error" key={rejection.file.name}>
+                <div className="file-upload__file is-rejected">
+                  {rejection.file.name}
+                  <Button
+                    appearance="base"
+                    className="file-upload__file-remove-button"
+                    onClick={() => removeRejectedFile(rejection)}
+                    type="button"
+                  >
+                    <Icon name="close">Remove file</Icon>
+                  </Button>
+                </div>
+                {rejection.errors.map((error) => (
+                  <p
+                    className="p-form-validation__message"
+                    key={`${rejection.file.name}-${error.code}`}
+                  >
+                    {error.message}
+                  </p>
+                ))}
+              </span>
+            ))}
+          {files &&
+            files.map((file) => (
+              <div className="file-upload__file" key={file.name}>
+                {file.name}
+                <Button
+                  appearance="base"
+                  className="file-upload__file-remove-button"
+                  onClick={() => removeFile(file)}
+                  type="button"
+                >
+                  <Icon name="close">Remove file</Icon>
+                </Button>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
+  );
+};
+
+export const FileUploadContainer = ({
+  accept,
+  error,
+  help,
+  label,
+  maxFiles,
+  maxSize,
+}: Pick<
+  FileUploadProps,
+  "accept" | "error" | "help" | "label" | "maxFiles" | "maxSize"
+>) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const [rejectedFiles, setRejectedFiles] = useState<FileRejection[]>([]);
+
+  const onFileUpload = useCallback(
+    (acceptedFiles: File[], fileRejections: FileRejection[]) => {
+      setFiles([...files, ...acceptedFiles]);
+      setRejectedFiles([...rejectedFiles, ...fileRejections]);
+    },
+    [files, rejectedFiles],
+  );
+
+  const removeFile = (file: File) => {
+    const newFiles = [...files];
+    newFiles.splice(newFiles.indexOf(file), 1);
+    setFiles(newFiles);
+  };
+
+  const removeRejectedFile = (fileRejection: FileRejection) => {
+    const newRejectedFiles = [...rejectedFiles];
+    newRejectedFiles.splice(newRejectedFiles.indexOf(fileRejection), 1);
+    setRejectedFiles(newRejectedFiles);
+  };
+
+  return (
+    <FileUpload
+      accept={accept}
+      error={error}
+      files={files}
+      rejectedFiles={rejectedFiles}
+      help={help}
+      label={label}
+      maxFiles={maxFiles}
+      maxSize={maxSize}
+      onFileUpload={onFileUpload}
+      removeFile={removeFile}
+      removeRejectedFile={removeRejectedFile}
+    />
   );
 };
