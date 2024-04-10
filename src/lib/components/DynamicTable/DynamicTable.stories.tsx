33a5da15f02a3da553294a2e15f3a7d2b0b2534a
Nick De Villiers <nick.devilliers@canonical.com>
2024-04-10 16:10:37 +0100

    fix: remove react table from story

--- src/lib/components/DynamicTable/DynamicTable.stories.tsx
+++ src/lib/components/DynamicTable/DynamicTable.stories.tsx
@@ -1,109 +1,19 @@
+import { Button, Icon } from "@canonical/react-components";
 import { Meta, StoryObj } from "@storybook/react";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
 
 import { DynamicTable } from "@/lib/components/DynamicTable/DynamicTable";
 
-type Device = {
-  fqdn: string;
-  ipAddress: string;
-  zone: string;
-  owner: string;
-};
-
-type DeviceColumnDef = ColumnDef<Device, Device[keyof Device]>;
-
-const columns: DeviceColumnDef[] = [
-  {
-    id: "fqdn",
-    accessorKey: "fqdn",
-    header: () => <div>FQDN</div>,
-  },
-  {
-    id: "ipAddress",
-    accessorKey: "ipAddress",
-    header: () => <div>IP address</div>,
-  },
-  {
-    id: "zone",
-    accessorKey: "zone",
-    header: () => <div>Zone</div>,
-  },
-  {
-    id: "owner",
-    accessorKey: "owner",
-    header: () => <div>Owner</div>,
-  },
-];
-
-const data = Array.from({ length: 50 }, (_, index) => ({
+const data = Array.from({ length: 25 }, (_, index) => ({
   fqdn: `machine-${index}`,
   ipAddress: `192.168.1.${index}`,
   zone: `zone-${index}`,
   owner: `owner-${index}`,
 }));
 
-const TableChildren = () => {
-  const table = useReactTable<Device>({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
-  return (
-    <>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <th
-                  className={`${header.column.id}`}
-                  colSpan={header.colSpan}
-                  key={header.id}
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext(),
-                  )}
-                </th>
-              );
-            })}
-          </tr>
-        ))}
-      </thead>
-      <DynamicTable.Body>
-        {table.getRowModel().rows.map((row) => {
-          return (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <td className={`${cell.column.id}`} key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </DynamicTable.Body>
-    </>
-  );
-};
-
 const meta: Meta<typeof DynamicTable> = {
   title: "components/DynamicTable",
   component: DynamicTable,
   tags: ["autodocs"],
-  render: () => (
-    <DynamicTable>
-      <TableChildren />
-    </DynamicTable>
-  ),
 };
 
 export default meta;
@@ -111,5 +21,36 @@ export default meta;
 export const Example: StoryObj<typeof DynamicTable> = {
   args: {
     className: "machines-table",
+    children: (
+      <>
+        <thead>
+          <tr>
+            <th>FQDN</th>
+            <th>IP address</th>
+            <th>Zone</th>
+            <th>Owner</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <DynamicTable.Body>
+          {data.map((item) => (
+            <tr key={item.fqdn}>
+              <td>{item.fqdn}</td>
+              <td>{item.fqdn}</td>
+              <td>{item.fqdn}</td>
+              <td>{item.fqdn}</td>
+              <td>
+                <Button
+                  appearance="base"
+                  style={{ marginBottom: 0, padding: 0 }}
+                >
+                  <Icon name="delete" />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </DynamicTable.Body>
+      </>
+    ),
   },
 };
