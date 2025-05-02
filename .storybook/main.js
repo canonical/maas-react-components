333150abb9bc30b344123fedc32621a9847bfe3a
Nick De Villiers <nick.devilliers@canonical.com>
2025-05-02 08:56:12 +0100

    fix: make sure all storybook addons have matched version, fix sass deprecations (#158)
    - Upgraded storybook and all plugins to 8.6.12
    - Ran `npx storybook@latest upgrade` to handle migrations and breaking changes
    - Upgraded canonical react components to latest version

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,20 +10,10 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "@etchteam/storybook-addon-status",
-    "sb-addon-permutation-table",
-    "@storybook/addon-mdx-gfm",
-    "storybook-addon-pseudo-states",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  features: {
-    storyStoreV7: true,
   },
 };
 
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,7 +5,6 @@ import { theme } from "./theme";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
