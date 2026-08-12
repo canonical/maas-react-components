e6d9eeb19b7dbbf4089d17c4c9338082eb23aeda
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-08-12 11:06:00 +0300

    feat(Layout): Lazy-loading generic Layout component MAASENG-6746 (#216)
    * fix(deps): type override to prevent import version mismatch
    
    * feat(Layout): added generic Layout component and auxiliaries
    
    * test(Layout): fixed test imports
    
    * test(Layout): fixed test providers
    
    * test(Layout): added new tests
    
    * fix(Layout): lib export and local scss import
    
    * test(Layout): proper lib import
    
    * fix(Layout): import vanilla

--- src/lib/sections/index.ts
+++ src/lib/sections/index.ts
@@ -1,7 +1,7 @@
 export * from "./ContentSection";
 export * from "./FormSection";
 export * from "./InputGroup";
+export * from "./Layout";
 export * from "./MainToolbar";
 export * from "./Navigation";
 export * from "./SidePanel";
-
