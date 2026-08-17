51a4e3a83f7c53f1d13dc69da400bec616565328
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-08-17 12:02:59 +0300

    fix(ExternalLink): updated import due to package changes

--- src/lib/elements/ExternalLink/ExternalLink.tsx
+++ src/lib/elements/ExternalLink/ExternalLink.tsx
@@ -1,8 +1,7 @@
 import { Link, LinkProps } from "@canonical/react-components";
-import type { LinkProps as RouterLinkProps } from "react-router-dom";
 
-type ExternalLinkProps = Pick<LinkProps, "children"> &
-  Omit<RouterLinkProps, "children"> & { to: string };
+type ExternalLinkProps = Omit<LinkProps, "href"> & { to: string };
+
 export const ExternalLink = ({ children, to, ...props }: ExternalLinkProps) => (
   <Link {...props} href={to} rel="noreferrer noopener" target="_blank">
     {children}
