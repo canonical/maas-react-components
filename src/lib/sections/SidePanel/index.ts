244b8c979aff65e561bdf956006ab616ce8c0ea8
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-08-19 18:34:17 +0300

    feat(Layout, SidePanel): side panel title rendering choice (#226)

--- src/lib/sections/SidePanel/index.ts
+++ src/lib/sections/SidePanel/index.ts
@@ -1,4 +1,4 @@
-export { SidePanel } from "./SidePanel";
+export { SidePanel, type SidePanelTitles } from "./SidePanel";
 export { SidePanelContextProvider, useSidePanel } from "./SidePanelContextProvider/SidePanelContextProvider";
 export { lazyLoadSidePanel } from "./utils";
 
